614fefbee84a9233c676396f2d0757bedcc15c63
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-11-08 21:29:26 -0500

    Add zlib fuzzer

---
+++ examples/zlib/fuzz.js
@@ -0,0 +1,36 @@
+const pako = require('pako')
+
+async function fuzz (bytes) {
+  try {
+    pako.deflate(bytes)
+    pako.inflate(bytes)
+  } catch (error) {
+    if (!acceptable(error)) throw error
+  }
+}
+
+function acceptable (error) {
+  return (
+    typeof error === 'string' &&
+    !!expected.find(message => error.startsWith(message))
+  )
+}
+
+const expected = [
+  'buffer error',
+  'incorrect header check',
+  'unknown compression method',
+  'invalid window size',
+  'invalid distance',
+  'invalid block type',
+  'invalid code lengths',
+  'too many length or distance symbols',
+  'invalid stored block lengths',
+  'need dictionary',
+  'incorrect data check',
+  'invalid literal/length code',
+  'invalid bit length repeat',
+  'invalid code'
+]
+
+exports.fuzz = fuzz
