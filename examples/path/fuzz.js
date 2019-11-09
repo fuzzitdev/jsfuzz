32ec7a44dbb5bb5a3aff61a77b4632962b1aab4c
Book Moons <35854232+bookmoons@users.noreply.github.com>
2019-11-08 20:29:35 -0500

    path: Remove unused error acceptor

--- examples/path/fuzz.js
+++ examples/path/fuzz.js
@@ -2,28 +2,15 @@ const path = require('path')
 
 async function fuzz (bytes) {
   const string = String.fromCodePoint(...bytes)
-  try {
-    path.basename(string)
-    path.dirname(string)
-    path.extname(string)
-    path.isAbsolute(string)
-    path.join(string)
-    path.normalize(string)
-    path.parse(string)
-    path.resolve(string)
-    path.toNamespacedPath(string)
-  } catch (error) {
-    if (!acceptable(error)) throw error
-  }
+  path.basename(string)
+  path.dirname(string)
+  path.extname(string)
+  path.isAbsolute(string)
+  path.join(string)
+  path.normalize(string)
+  path.parse(string)
+  path.resolve(string)
+  path.toNamespacedPath(string)
 }
-
-function acceptable (error) {
-  return !!expected
-    .find(message => error.message.startsWith(message))
-}
-
-const expected = [
-  
-]
 
 exports.fuzz = fuzz
